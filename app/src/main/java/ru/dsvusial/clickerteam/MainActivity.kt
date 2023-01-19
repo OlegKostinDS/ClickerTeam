2567745d7b089644c179de029a0b46c41c4cc527
OlegKostinDS <olegkostinds@gmail.com>
2023-01-19 21:21:39 +0300

    second anim commit

--- app/src/main/java/ru/dsvusial/clickerteam/MainActivity.kt
+++ app/src/main/java/ru/dsvusial/clickerteam/MainActivity.kt
@@ -1,45 +1,51 @@
 package ru.dsvusial.clickerteam
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.AnimatorSet
+import android.os.Build
 import android.os.Bundle
-import android.view.View
+import android.view.animation.AnimationSet
 import android.view.animation.AnimationUtils
+import android.widget.FrameLayout
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val pigBtn = findViewById<ImageButton>(R.id.pig_btn)
-        val crocBtn = findViewById<ImageButton>(R.id.croc_btn)
-        crocBtn.isEnabled = false
-        crocBtn.visibility = View.INVISIBLE
+        val gameField = findViewById<FrameLayout>(R.id.game_field)
+
+
+
+
+
         pigBtn.setOnClickListener {
             val anim = AnimationUtils.loadAnimation(
-                this, R.anim.anim
+                this, R.anim.hide_anim
             )
             val secondAnim = AnimationUtils.loadAnimation(
-                this, R.anim.second_anim
+                this, R.anim.reveal_anim
             )
-            it.isEnabled = false
-            it.startAnimation(anim)
-            crocBtn.startAnimation(secondAnim)
-            crocBtn.visibility = View.VISIBLE
-            crocBtn.isEnabled = true
-        }
-        crocBtn.setOnClickListener {
-            val anim = AnimationUtils.loadAnimation(
-                this, R.anim.anim
-            )
-            val secondAnim = AnimationUtils.loadAnimation(
-                this, R.anim.second_anim
-            )
-            it.isEnabled = false
-            it.startAnimation(anim)
+
+            val width: Int = pigBtn.measuredWidth
+            val height: Int = pigBtn.measuredHeight
+
+            //pigBtn.startAnimation(anim)
+            val an = AnimationUtils.loadAnimation(this, R.anim.hide_anim)
+            pigBtn.startAnimation(an)
+            pigBtn.translationX = Random.nextInt(-width, width).toFloat()
+            pigBtn.translationY = Random.nextInt(-height, height).toFloat()
+            Toast.makeText(this, "$width $height", Toast.LENGTH_LONG).show()
             pigBtn.startAnimation(secondAnim)
-            pigBtn.visibility = View.VISIBLE
-            pigBtn.isEnabled = true
+//            it.isEnabled = false
+//            it.startAnimation(anim)
+
 
         }
+
     }
 }
